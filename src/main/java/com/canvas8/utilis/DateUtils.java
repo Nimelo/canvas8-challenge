71c91e7a2dcec4af16254eccf17a44b4739bdf8d
Mateusz Gasior <mr.nimelo@gmail.com>
2017-05-02 00:15:38 +0100

    Supporting new user created date

--- src/main/java/com/canvas8/utilis/DateUtils.java
+++ src/main/java/com/canvas8/utilis/DateUtils.java
@@ -8,7 +8,7 @@ import java.util.Date;
 public class DateUtils {
     public static boolean isAfterCurrentTime(Date date){
         if(date == null){
-            return false;
+            return true;
         }
         Date currentDate = new Date();
         return date.after(currentDate);
